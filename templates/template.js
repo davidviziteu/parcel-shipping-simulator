3760bb0ccf111b42a7f206a96b99de988619f4df
bogdan.bujor <bujorbogdanconstantin@gmail.com>
2021-03-26 21:22:16 +0200

    Update template

--- templates/template.js
+++ templates/template.js
@@ -2,8 +2,25 @@ let hamburgerMenu = document.getElementById(`hamburger`)
 let menu = document.getElementById(`menu`)
 
 hamburgerMenu.addEventListener(`click`, () => {
-    if (menu.className.includes(`hidden`))
-        menu.className = ``
-    else
+    if (menu.className.includes(`hidden`)) {
+        menu.className = ``;
+        disableScroll();
+    }
+    else {
         menu.className = `hidden`
+        enableScroll();
+    }
 })
+
+function disableScroll() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+        window.onscroll = function () {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+function enableScroll() {
+    window.onscroll = function () { };
+}
