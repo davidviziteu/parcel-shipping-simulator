2b20b8f93ca3164f8a78560087268201bc9df339
bogdan.bujor <bujorbogdanconstantin@gmail.com>
2021-04-01 12:12:44 +0300

    New Register-fromTemplate

--- public/CreateAccount.js
+++
@@ -1,9 +0,0 @@
-var firstName, lastName, email, password;
-
-function information() {
-    firstName = document.getElementById("fname").value;
-    lastName = document.getElementById("lname").value;
-    email = document.getElementById("email").value;
-    password = document.getElementById("pwd").value;
-    console.log(firstName);
-}
