f6bbed0d243065d486ff815fc7f392c6bed236c9
AndreeaVina <vinaandreea27@gmail.com>
2021-04-04 20:50:13 +0300

    new order in work

--- public/scripts/NewOrder.js
+++ public/scripts/NewOrder.js
@@ -1,3 +1,12 @@
-function checkAccount() {
-    window.open("NewOrder2.html", "_self")
+var judetSelector = document.getElementById("judet");
+var localitateSelector = document.getElementById("localitate");
+
+for (var judet in cities) {
+    judetSelector.options[judetSelector.options.length] = new Option(judet, judet);
+}
+judetSelector.onchange = function() {   
+    localitateSelector.length = 1;
+    for (var localitate in cities[this.value]) {
+        localitateSelector.options[localitateSelector.options.length] = new Option(cities[this.value][localitate]);
+    }
 }
