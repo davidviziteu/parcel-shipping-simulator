4699b65434622eb2f238e7020f146abc10f687c3
bogdan.bujor <bujorbogdanconstantin@gmail.com>
2021-04-04 10:52:09 +0300

    Merge sa vezi statistica pe fiecare oras

--- public/scripts/statistici.js
+++ public/scripts/statistici.js
@@ -10,12 +10,37 @@ function numeButoane() {
 function statisticaNoua(value) {
     document.getElementById("none").style.display = "block"
     var children = document.getElementsByClassName("button-container")[1].children;
-    for (var i = 0; i < children.length;)console.log(children[i].remove());
+    for (var i = 0; i < children.length;)children[i].remove();
     for (local in cities[value]) {
         var btn = document.createElement("BUTTON");
         var x = document.getElementsByClassName("button-container")[1].appendChild(btn);
         x.innerHTML = cities[value][local];
     }
+    google.charts.setOnLoadCallback(drawChart(value));
 }
 
 window.onload = function () { numeButoane() }
+
+google.charts.load('current', { 'packages': ['corechart'] });
+
+function drawChart(value) {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Location');
+    data.addColumn('number', 'Number of orders');
+
+    for (local in cities[value]) {
+        data.addRows([
+            [cities[value][local], 2.5]
+        ])
+    }
+
+    var options = {
+        title: 'Statistica pe orasul ' + value,
+        backgroundColor: 'transparent',
+        height: '400',
+        is3D: 'true'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('myChart'));
+    chart.draw(data, options);
+}
