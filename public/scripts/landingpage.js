e951ecbe07f65a449e0580b6006669ff19d3aaea
David Viziteu <andrei_david7@hotmail.com>
2021-04-11 18:27:13 +0300

    many bug-fixes; added admin-dashboard

--- public/scripts/landingpage.js
+++ public/scripts/landingpage.js
@@ -11,12 +11,11 @@ for (let element in cities) {
 estimateCost.addEventListener(`click`, () => {
     let from = sourceSelector.value
     let to = destinationSelector.value
-    console.log(`${from} -> ${to}: 35 RON`)
-    if (to.startsWith(`Alege`) && from.startsWith(`Alege`))
+    if (!to && !from)
         totalCost.innerHTML = `Alegeți județul expeditorului și al destinatarului`
-    else if (from.startsWith(`Alege`))
+    else if (!from)
         totalCost.innerHTML = `Alegeți județul expeditorului`
-    else if (to.startsWith(`Alege`))
+    else if (!to)
         totalCost.innerHTML = `Alegeți județul destinatarului`
     else if (to == from)
         totalCost.innerHTML = `Expediere în același județ (${from}): aproximativ 20 RON`
@@ -25,9 +24,3 @@ estimateCost.addEventListener(`click`, () => {
 
 
 document.getElementById(`our-team`).onclick = () => location.href = `AboutUs.html`
-document.getElementById(`reset-password`).onclick = () => location.href = `ResetPassword.html`
-document.getElementById(`register`).onclick = () => location.href = `register.html`
-document.getElementById(`demo-client`).onclick = () => location.href = `DashboardClient.html`
-document.getElementById(`demo-admin`).onclick = () => location.href = `dashboard.html`
-document.getElementById(`demo-sofer`).onclick = () => location.href = `DashboardSofer.html`
-document.getElementById(`track-awb`).onclick = () => location.href = `DashboardClient.html`
