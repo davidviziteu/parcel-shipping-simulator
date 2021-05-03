d37bbdbf658626bdecf8a256b02a905679efc4e6
AndreeaVina <vinaandreea27@gmail.com>
2021-05-04 01:55:45 +0300

    added example

--- routes/common.js
+++ routes/common.js
@@ -1,10 +1,20 @@
 const { Router } = require(`../utils/router.js`)
 console.log("aici")
+let dbAWB = [1,2,3];
 let router = new Router();
-router.get(`/AWB`, (req, res) => {
-    if (req.data) {
-        console.log(req.data);
+router.get(`/api/AWB`, (req, res) => {
+    console.log(req.data)
+    if (req.data.AWB) {
+       if(dbAWB.find(function(arg){
+           return arg == req.data.AWB;
+       }))
+            return res.status(200).json({
+                "succes":true
+            })
+        else return res.status(200).json({
+            "succes":false
+        })
     }
-    return res;
+    return res.status(300)
 });
 module.exports = {router}
