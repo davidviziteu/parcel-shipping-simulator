7fa803e39fd9320e744ef35a0887894d9f3ede03
bogdanbujor99 <bujorbogdanconstantin@gmail.com>
2021-05-05 12:41:34 +0300

    add function for db

---
+++ models/model.js
@@ -0,0 +1,70 @@
+const pool = require("../config/database")
+
+module.exports = {
+    getAwbEvents: (awb, callBack) => {
+        pool.query(
+            `select * from awb_events where awb = ?`, [awb],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
+    },
+    getUserByEmail: (email, callBack) => {
+        pool.query(
+            `select * from users where email = ?`, [email],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
+    },
+    getSoferByEmail: (email, callBack) => {
+        pool.query(
+            `select * from soferi where email = ?`, [email],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
+    },
+    getAngajatByEmail: (email, callBack) => {
+        pool.query(
+            `select * from angajati where email = ?`, [email],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
+    },
+    getAdminByEmail: (email, callBack) => {
+        pool.query(
+            `select * from admini where email = ?`, [email],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
+    },
+    getTokens: (id, callBack) => {
+        pool.query(
+            `select * from tokens where id = ?`, [id],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
+    }
+}
