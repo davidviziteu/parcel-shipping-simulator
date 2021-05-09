2643e4418fb053c5b69b8676d8b90ddd18e54065
David Viziteu <andrei_david7@hotmail.com>
2021-05-09 12:05:02 +0300

    login controller and code refactoring

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-const newUserSchema = Joi.object().keys({
+exports.newUserSchema = Joi.object().keys({
     fname: Joi.string().required(),
     lname: Joi.string().required(),
     citySelect: Joi.string().required(),
@@ -11,4 +11,7 @@ const newUserSchema = Joi.object().keys({
     phone: Joi.string().length(10).pattern(/^[0-9]+$/).required()
 });
 
-module.exports = newUserSchema
+exports.loginUserSchema = Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+})
