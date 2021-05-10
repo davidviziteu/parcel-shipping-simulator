6a5727f2dad5f8bb78b2539103add4e9e555108c
AndreeaVina <vinaandreea27@gmail.com>
2021-05-10 19:38:05 +0300

    last validation for REST API

--- models/validationNewOrder.js
+++ models/validationNewOrder.js
@@ -2,16 +2,16 @@ const Joi = require('joi');
 
 
 const newOrderSchema = Joi.object().options({ abortEarly: false }).keys({
-    fullName_sender: Joi.string().required(),
-    contactPerson_sender: Joi.string().regex(/^([a-zA-Z]+\s)*[a-zA-Z]+$/).required(),
+    fullName_sender: Joi.string().regex(/^[a-z][a-z\s]*$/).required(),
+    contactPerson_sender: Joi.string().regex(/^[a-z][a-z\s]*$/).required(),
     phone_sender: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
     email_sender: Joi.string().email().required(),
-    county_sender: Joi.string().valid("Ilfov", "Cluj", "Constanța", "Dolj", "Galați", "Iași", "Oradea", "Sibiu", "Timișoara"),
+    county_sender: Joi.string().valid('Ilfov', 'Cluj', 'Constanța', 'Dolj', 'Galați', 'Iași', 'Oradea', 'Sibiu', 'Timișoara').required(),
     country_sender: Joi.string().required().valid("Cluj-Napoca", "Câmpia Turzii", "Dej", "Gherla", "Huedin", "Turda", "Constanţa", "Băneasa", "Cernavodă", "Eforie", "Hârşova", "Mangalia", "Medgidia", "Murfatlar", "Năvodari", "Negru Vodă", "Ovidiu", "Techirghiol", "Craiova", "Băileşti", "Bechet", "Calafat", "Dăbuleni", "Filiaşi", "Segarcea", "Galaţi", "Beresti", "Târgu Bujor", "Tecuci", "Iaşi", "Hârlău", "Paşcani", "Podu Iloaiei", "Târgu Frumos", "București", "Bragadiru", "Buftea", "Chitila", "Măgurele", "Otopeni", "Pantelimon", "Popeşti - Leordeni", "Voluntari", "Oradea", "Aleşd", "Beiuş", "Marghita", "Nucet", "Salonta", "Săcuieni", "Ştei", "Valea lui Mihai", "Vaşcău", "Sibiu", "Agnita", "Avrig", "Cisnădie", "Copşa Mică", "Dumbrăveni", "Mediaş", "Miercurea Sibiului", "Ocna Sibiului", "Sălişte", "Tălmaciu", "Timișoara", "Buziaș", "Ciacova", "Deta", "Făget", "Gătaia", "Jimbolia", "Lugoj", "Recaș", "Sâncicolau Mare"),
     address_sender: Joi.string().required(),
 
-    fullName_receiver: Joi.string().required(),
-    contactPerson_receiver: Joi.string().required(),
+    fullName_receiver: Joi.string().regex(/^[a-z][a-z\s]*$/).required(),
+    contactPerson_receiver: Joi.string().regex(/^[a-z][a-z\s]*$/).required(),
     phone_receiver: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
     county_receiver: Joi.string().valid('Ilfov', 'Cluj', 'Constanța', 'Dolj', 'Galați', 'Iași', 'Oradea', 'Sibiu', 'Timișoara').required(),
     country_receiver: Joi.string().required().valid("Cluj-Napoca", "Câmpia Turzii", "Dej", "Gherla", "Huedin", "Turda", "Constanţa", "Băneasa", "Cernavodă", "Eforie", "Hârşova", "Mangalia", "Medgidia", "Murfatlar", "Năvodari", "Negru Vodă", "Ovidiu", "Techirghiol", "Craiova", "Băileşti", "Bechet", "Calafat", "Dăbuleni", "Filiaşi", "Segarcea", "Galaţi", "Beresti", "Târgu Bujor", "Tecuci", "Iaşi", "Hârlău", "Paşcani", "Podu Iloaiei", "Târgu Frumos", "București", "Bragadiru", "Buftea", "Chitila", "Măgurele", "Otopeni", "Pantelimon", "Popeşti - Leordeni", "Voluntari", "Oradea", "Aleşd", "Beiuş", "Marghita", "Nucet", "Salonta", "Săcuieni", "Ştei", "Valea lui Mihai", "Vaşcău", "Sibiu", "Agnita", "Avrig", "Cisnădie", "Copşa Mică", "Dumbrăveni", "Mediaş", "Miercurea Sibiului", "Ocna Sibiului", "Sălişte", "Tălmaciu", "Timișoara", "Buziaș", "Ciacova", "Deta", "Făget", "Gătaia", "Jimbolia", "Lugoj", "Recaș", "Sâncicolau Mare"),
@@ -29,7 +29,7 @@ const newOrderSchema = Joi.object().options({ abortEarly: false }).keys({
     preference1: Joi.boolean(),
     preference2: Joi.boolean(),
     preference3: Joi.boolean(),
-    payment: Joi.string().required(),
+    payment: Joi.string().valid('cash', 'card').required(),
     mentions: Joi.string().required().allow(null, '')
 });
 
