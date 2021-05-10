7a463fb1f3f4d86cf5b9940098b4f8476448c1ed
bogdanbujor99 <bujorbogdanconstantin@gmail.com>
2021-05-10 11:09:22 +0300

    solving bug front

--- public/scripts/NewOrder2.js
+++ public/scripts/NewOrder2.js
@@ -21,7 +21,7 @@ judetSelector1.onchange = function () {
 }
 for (var judet in cities) {
     judetSelector2.options[judetSelector2.options.length] = new Option(judet, judet);
-    
+
 }
 judetSelector2.onchange = function () {
     localitateSelector2.length = 1;
@@ -33,49 +33,49 @@ for (var mod in metodePlata) {
     plata.options[plata.options.length] = new Option(metodePlata[mod]);
 
 }
-window.onload= function() {
+window.onload = function () {
     document.getElementById("preference1").checked = false;
     document.getElementById("preference2").checked = false;
     document.getElementById("preference3").checked = false;
 }
-form.onsubmit = async(e)=>{
+form.onsubmit = async (e) => {
     e.preventDefault();
     var values = {
-        fullName_sender : document.getElementById("expName").value,
-        contactPerson_sender : document.getElementById("expContactName").value,
-        phone_sender :document.getElementById("expPhone").value,
-        email_sender :document.getElementById("expEmail").value,
-        county_sender : document.getElementById("judet1").value,
-        country_sender : document.getElementById("localitate1").value,
-        address_sender : document.getElementById("expAddress").value,
-     
-        fullName_receiver : document.getElementById("destName").value,
-        contactPerson_receiver : document.getElementById("destContactName").value,
-        phone_receiver : document.getElementById("destPhone").value,
-        county_receiver : document.getElementById("judet2").value,
-        country_receiver : document.getElementById("localitate2").value,
-        address_receiver : document.getElementById("destAddress").value,
-     
-        nrEnvelope : document.getElementById("envelope").value,
-        nrParcel : document.getElementById("parcel").value,
-        weight : document.getElementById("weight").value,
-     
-        length :document.getElementById("length").value,
-        width : document.getElementById("width").value,
-        height :document.getElementById("height").value,
-     
-        date : document.getElementById("date").value,
-        hour : document.getElementById("hour").value,
-     
-        preference1 : document.getElementById("preference1").checked,
-        preference2 :document.getElementById("preference2").checked,
-        preference3 : document.getElementById("preference3").checked,
-     
-        payment :document.getElementById("payment").value,
-     
-        mentions : document.getElementById("mentions").value
-     }
-     fetch('http://localhost:4000/api/neworder', {
+        fullName_sender: document.getElementById("expName").value,
+        contactPerson_sender: document.getElementById("expContactName").value,
+        phone_sender: document.getElementById("expPhone").value,
+        email_sender: document.getElementById("expEmail").value,
+        county_sender: document.getElementById("judet1").value,
+        country_sender: document.getElementById("localitate1").value,
+        address_sender: document.getElementById("expAddress").value,
+
+        fullName_receiver: document.getElementById("destName").value,
+        contactPerson_receiver: document.getElementById("destContactName").value,
+        phone_receiver: document.getElementById("destPhone").value,
+        county_receiver: document.getElementById("judet2").value,
+        country_receiver: document.getElementById("localitate2").value,
+        address_receiver: document.getElementById("destAddress").value,
+
+        nrEnvelope: document.getElementById("envelope").value,
+        nrParcel: document.getElementById("parcel").value,
+        weight: document.getElementById("weight").value,
+
+        length: document.getElementById("length").value,
+        width: document.getElementById("width").value,
+        height: document.getElementById("height").value,
+
+        date: document.getElementById("date").value,
+        hour: document.getElementById("hour").value,
+
+        preference1: document.getElementById("preference1").checked,
+        preference2: document.getElementById("preference2").checked,
+        preference3: document.getElementById("preference3").checked,
+
+        payment: document.getElementById("payment").value,
+
+        mentions: document.getElementById("mentions").value
+    }
+    fetch('http://localhost:4000/api/neworder', {
         method: "POST",
         body: JSON.stringify(values),
         headers: { "Content-type": "application/json; charset=UTF-8" }
@@ -85,7 +85,7 @@ form.onsubmit = async(e)=>{
             console.log(json.error);
             if (json.error.includes('phone_receiver')) {
                 console.log("yes");
-                document.getElementById("destEmail").style.backgroundColor="red";
+                document.getElementById("destPhone").style.backgroundColor = "red";
             }
         })
         .catch(err => { console.log(err) });
