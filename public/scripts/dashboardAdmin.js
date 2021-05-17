90186bc635d01ebfac4815a15d3132e39eb66ed5
AndreeaVina <vinaandreea27@gmail.com>
2021-05-17 19:29:14 +0300

    add new notification

--- public/scripts/dashboardAdmin.js
+++ public/scripts/dashboardAdmin.js
@@ -1,12 +1,9 @@
 const newAccoutButton = document.getElementById(`new-account-button`)
 const addNotificationtButton = document.getElementById(`add-notification-button`)
-
+const addNotificationForm = document.getElementById(`add-notification-form`);
 window.addEventListener(`api-fetched`, (ev) => {
 
-    var values = {
-        text: document.getElementById(`new-notification-text`).value,
-        expiry_date: getElementById(`new-notification-date`).value
-    }
+
     // newAccoutButton.addEventListener(`click`, async() => {
     //         fetch(`${hostName}${api.newAccount.route}`, {
     //                 method: api.newAccount.method,
@@ -20,18 +17,36 @@ window.addEventListener(`api-fetched`, (ev) => {
     //             .then(json => handleLoginResponse(json))
     //             .catch(err => { alert(err) });
     //     })
-    addNotificationtButton.addEventListener(`click`, async () => {
+    // addNotificationtButton.addEventListener(`click`, async () => {
+    //     fetch(`${hostName}${api.addNotification.route}`, {
+    //         method: api.addNotification.method,
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         withCredentials: true,
+    //         body: JSON.stringify(values),
+    //     })
+    //         .then(response => response.json())
+    //         // .then(json => handleLoginResponse(json))
+    //         .catch(err => { alert(err) });
+    // })
+    addNotificationForm.onsubmit = async(e) => {
+        e.preventDefault();
+        var values = {
+            text: document.getElementById(`new-notification-text`).value,
+            expiry_date: document.getElementById(`new-notification-date`).value
+        }
         fetch(`${hostName}${api.addNotification.route}`, {
-            method: api.addNotification.method,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            withCredentials: true,
-            body: JSON.stringify(values),
-        })
+                method: api.addNotification.method,
+                body: JSON.stringify(values),
+                headers: { "Content-type": "application/json; charset=UTF-8" }
+            })
             .then(response => response.json())
-            // .then(json => handleLoginResponse(json))
-            .catch(err => { alert(err) });
-    })
+            .then(json => {
+
+            })
+            .catch(err => { console.log(err) });
+    }
+
 
 }, false)
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,11 @@
 const { Router } = require("../utils/router");
+const controllers = require("../controller");
+const api = require("../models").apiModel;
+const adminController = require("../controller/adminController");
 
 const adminRouter = new Router();
+adminRouter.post(api.adminApi.addNotification.route, adminController.addNotification);
+
 
 
 
