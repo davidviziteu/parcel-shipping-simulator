1d84e3afba4f8928d3a73855f53345cde6d6ee70
bogdanbujor99 <bujorbogdanconstantin@gmail.com>
2021-05-31 13:43:41 +0300

    add new info in token and solving bug register

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -3,7 +3,7 @@ const { Router } = require("../utils/router");
 const api = require("../models/apiModel")
 
 const clientRouter = new Router();
-clientRouter.post(`/api/register`, controllers.clientController.createAccountUser);
+clientRouter.post(api.baseApi.newAccount.route, controllers.clientController.createAccountUser);
 clientRouter.get(`/api/neworder`, controllers.clientController.getCost);
 clientRouter.post(api.baseApi.newOrder.route, controllers.clientController.placeOrder);
 clientRouter.post(api.baseApi.getCode.route, controllers.clientController.codeChange);
