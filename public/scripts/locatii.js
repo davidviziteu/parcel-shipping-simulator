598e1462b56dfccf55da46cc38e6360dc98051ae
bogdanbujor99 <bujorbogdanconstantin@gmail.com>
2021-06-07 23:07:09 +0300

    solving bug locatii.html

---
+++ public/scripts/locatii.js
@@ -0,0 +1,39 @@
+document.getElementById('ilfov').addEventListener('click', () => {
+    document.getElementsByTagName('ul')[0].toggleAttribute('hidden')
+})
+
+document.getElementById('cluj').addEventListener('click', () => {
+    document.getElementsByTagName('ul')[1].toggleAttribute('hidden')
+})
+
+document.getElementById('constanta').addEventListener('click', () => {
+    document.getElementsByTagName('ul')[2].toggleAttribute('hidden')
+})
+
+document.getElementById('dolj').addEventListener('click', () => {
+    document.getElementsByTagName('ul')[3].toggleAttribute('hidden')
+})
+
+document.getElementById('galati').addEventListener('click', () => {
+    document.getElementsByTagName('ul')[4].toggleAttribute('hidden')
+})
+
+document.getElementById('iasi').addEventListener('click', () => {
+    document.getElementsByTagName('ul')[5].toggleAttribute('hidden')
+})
+
+document.getElementById('oradea').addEventListener('click', () => {
+    document.getElementsByTagName('ul')[6].toggleAttribute('hidden')
+})
+
+document.getElementById('sibiu').addEventListener('click', () => {
+    document.getElementsByTagName('ul')[7].toggleAttribute('hidden')
+})
+
+document.getElementById('timisoara').addEventListener('click', () => {
+    document.getElementsByTagName('ul')[8].toggleAttribute('hidden')
+})
+
+document.getElementById('arad').addEventListener('click', () => {
+    document.getElementsByTagName('ul')[9].toggleAttribute('hidden')
+})
